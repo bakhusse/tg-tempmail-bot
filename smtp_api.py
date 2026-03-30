786b0face16e05a0cfebcb01fa563627e9721991
bakhusse <98408944+bakhusse@users.noreply.github.com>
2026-03-30 23:51:11 +0700

    Update smtp_api.py

--- smtp_api.py
+++ smtp_api.py
@@ -10,11 +10,9 @@ class SMTPDev:
             r = await client.get(f"{self.BASE_URL}/domains?isActive=true", headers=self.HEADERS)
             data = r.json()
             
-            # Если API вернуло словарь с ключом 'member' (как в доке)
             if isinstance(data, dict):
                 return data.get('member', [])
-            
-            # Если API вернуло сразу список (как в твоем случае)
+
             elif isinstance(data, list):
                 return data
                 
@@ -28,11 +26,9 @@ class SMTPDev:
 
     async def get_messages(self, account_id):
         async with httpx.AsyncClient() as client:
-            # 1. Получаем список ящиков (mailboxes)
             box_r = await client.get(f"{self.BASE_URL}/accounts/{account_id}/mailboxes", headers=self.HEADERS)
             mailboxes = box_r.json()
-            
-            # Если пришел словарь с 'member', берем его, иначе считаем, что это список
+
             if isinstance(mailboxes, dict):
                 mailboxes = mailboxes.get('member', [])
                 
@@ -41,9 +37,8 @@ class SMTPDev:
                 inbox = next(m for m in mailboxes if m['path'] == 'INBOX')
                 inbox_id = inbox['id']
             except StopIteration:
-                return [], None # Если инбокс почему-то не найден
+                return [], None
 
-            # 2. Получаем сообщения
             msg_r = await client.get(f"{self.BASE_URL}/accounts/{account_id}/mailboxes/{inbox_id}/messages", headers=self.HEADERS)
             messages = msg_r.json()
             
